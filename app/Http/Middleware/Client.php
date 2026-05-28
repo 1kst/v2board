b95084fa36266147a7a9128427d26d443467e102
1kst <fxcktg@gmail.com>
2026-05-28 14:26:49 +0800

    Update Client.php

--- app/Http/Middleware/Client.php
+++ app/Http/Middleware/Client.php
@@ -20,7 +20,8 @@ class Client
     {
         $token = $request->input('token');
         if (empty($token)) {
-            abort(403, 'token is null');
+            // 修改点 1：token 为空
+            return redirect()->away('https://www.baidu.com');
         }
         $submethod = (int)config('v2board.show_subscribe_method', 0);
         switch ($submethod) {
@@ -28,7 +29,8 @@ class Client
                 break;
             case 1:
                 if (!Cache::has("otpn_{$token}")) {
-                    abort(403, 'token is error');
+                    // 修改点 2：OTP token 错误
+                    return redirect()->away('https://www.baidu.com');
                 }
                 $usertoken = Cache::pull("otpn_{$token}");
                 Cache::forget("otp_{$usertoken}");
@@ -42,21 +44,25 @@ class Client
                     $counterBytes = pack('N*', 0) . pack('N*', $counter);
                     $idhash = Helper::base64DecodeUrlSafe($token);
                     if (strpos($idhash, ':') === false) {
-                        abort(403, 'token is error');
+                        // 修改点 3：TOTP token 格式错误
+                        return redirect()->away('https://www.baidu.com');
                     }
                     $parts = explode(':', $idhash, 2);
                     [$userid, $clienthash] = $parts;
                     if (!$userid || !$clienthash) {
-                        abort(403, 'token is error');
+                        // 修改点 4：TOTP token 格式错误
+                        return redirect()->away('https://www.baidu.com');
                     }
                     $user = User::where('id', $userid)->select('token')->first();
                     if (!$user) {
-                        abort(403, 'token is error');
+                        // 修改点 5：TOTP token 中的用户 ID 错误
+                        return redirect()->away('https://www.baidu.com');
                     }
                     $usertoken = $user->token;
                     $hash = hash_hmac('sha1', $counterBytes, $usertoken, false);
                     if ($clienthash !== $hash) {
-                        abort(403, 'token is error');
+                        // 修改点 6：TOTP token 哈希验证失败
+                        return redirect()->away('https://www.baidu.com');
                     }
                     Cache::put("totp_{$token}", $usertoken, $timestep);
                 }
@@ -67,11 +73,14 @@ class Client
         }
         $user = User::where('token', $token)->first();
         if (!$user) {
-            abort(403, 'token is error');
+            // 修改点 7：最终的 token 在数据库中找不到
+            return redirect()->away('https://www.baidu.com');
         }
         $request->merge([
             'user' => $user
         ]);
+        
+        // 只有这里是唯一放行的出口
         return $next($request);
     }
 }
